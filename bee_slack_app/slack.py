b303f5335a9cb169d39ab98d509c6066840a2ae2
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2022-04-12 18:25:50 +0900

    DynamoDBを設定

--- bee_slack_app/slack.py
+++ bee_slack_app/slack.py
@@ -5,6 +5,7 @@ import boto3  # type: ignore
 from slack_bolt import App
 
 from bee_slack_app import ml
+from bee_slack_app.repository import bookReview
 
 secretsmanager_client = boto3.client("secretsmanager", region_name="us-east-1")
 secret_value = secretsmanager_client.get_secret_value(SecretId="slack_secret")
@@ -16,6 +17,8 @@ os.environ["SLACK_SIGNING_SECRET"] = secret["SLACK_SIGNING_SECRET"]
 
 
 app = App(process_before_response=True)
+
+dynamodb = boto3.resource("dynamodb")
 
 
 @app.message("hello")
@@ -30,3 +33,10 @@ def message_predict(_, say):
     predicted = ml.predict()
 
     say(f"predicted = {predicted}")
+
+
+@app.message("database-test")
+def db_create(_, say):
+    item = bookReview.create()
+
+    say(f"アイテムの作成が成功しました: {item}")
