a3f00fd4e5e81fc2b9245c28df624678a58698c9
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-04-12 09:45:01 +0000

    AWS依存部分をapp.pyに集約
    Localで動かす場合は
    app_local.flask_app
    を直接呼び出す

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
+import json
+import os
+
 import logging
 
 import awsgi  # type: ignore
-from flask import Flask, request
-from slack_bolt.adapter.flask import SlackRequestHandler
-
-# TODO: slack.pyの型情報をmypyで参照できるようにする
-from bee_slack_app import slack  # type: ignore
+import app_local
+import boto3  # type: ignore
 
 logging.basicConfig(level=logging.DEBUG)
 
+secretsmanager_client = boto3.client("secretsmanager", region_name="us-east-1")
+secret_value = secretsmanager_client.get_secret_value(SecretId="slack_secret")
+secret = json.loads(secret_value["SecretString"])
 
-flask_app = Flask(__name__)
+os.environ["SLACK_APP_TOKEN"] = secret["SLACK_APP_TOKEN"]
+os.environ["SLACK_BOT_TOKEN"] = secret["SLACK_BOT_TOKEN"]
+os.environ["SLACK_SIGNING_SECRET"] = secret["SLACK_SIGNING_SECRET"]
 
-
-handler = SlackRequestHandler(slack.app)
-
-
-@flask_app.route("/slack/events", methods=["POST"])
-def slack_events():
-    return handler.handle(request)
-
+flask_app = app_local.flask_app
 
 def lambda_handler(event, context):
     return awsgi.response(flask_app, event, context)
