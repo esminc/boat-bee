331f1dd293124dd201f75fa9752869e5ebca1e30
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-04-13 06:44:52 +0000

    アプリ生成の前に環境変数を設定する

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import os
 import awsgi  # type: ignore
 import boto3  # type: ignore
 
-from bee_slack_app.flask_app import flask_app
-
 logging.basicConfig(level=logging.DEBUG)
 
 secretsmanager_client = boto3.client("secretsmanager", region_name="us-east-1")
@@ -16,6 +14,8 @@ secret = json.loads(secret_value["SecretString"])
 os.environ["SLACK_APP_TOKEN"] = secret["SLACK_APP_TOKEN"]
 os.environ["SLACK_BOT_TOKEN"] = secret["SLACK_BOT_TOKEN"]
 os.environ["SLACK_SIGNING_SECRET"] = secret["SLACK_SIGNING_SECRET"]
+
+from bee_slack_app.flask_app import flask_app
 
 
 def lambda_handler(event, context):
