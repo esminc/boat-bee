1c0e2fa520c120abf61656e648c6db7544695df1
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-04-14 01:47:59 +0000

    レビューコマンドでボタンを表示する

--- bee_slack_app/slack.py
+++ bee_slack_app/slack.py
@@ -13,8 +13,20 @@ def message_hello(message, say):
 @app.message("レビュー")
 def message_review(message, say):
     # say() sends a message to the channel where the event was triggered
-    say(f"レビューします <@{message['user']}>!")
-
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"レビューします <@{message['user']}>!"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text":"Click Me"},
+                    "action_id": "button_click"
+                }
+            }
+        ],
+        text=f"Hey there <@{message['user']}>!"
+    )
 @app.message("predict")
 def message_predict(_, say):
 
