6475220dcde88e6ee13cc57dda260075c5677fad
mitamurat <mita@esm.co.jp>
2022-04-14 01:32:35 +0000

    レビューコマンド追加

--- bee_slack_app/slack.py
+++ bee_slack_app/slack.py
@@ -10,6 +10,10 @@ def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>!")
 
+@app.message("レビュー")
+def message_review(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"レビューします <@{message['user']}>!")
 
 @app.message("predict")
 def message_predict(_, say):
