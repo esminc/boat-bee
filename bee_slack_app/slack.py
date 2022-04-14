15b1c6ae4b6ccaa67e8babfbaddc3a52adf2f0d7
k-misawa <k-misawa@esm.co.jp>
2022-04-14 02:11:57 +0000

    レビューボタン押下でモーダル画面を表示する

--- bee_slack_app/slack.py
+++ bee_slack_app/slack.py
@@ -29,9 +29,43 @@ def message_review(message, say):
     )
 
 @app.action("button_click")
-def action_button_click(body, ack, say):
+def open_modal(ack, body, client):
+    # コマンドのリクエストを確認
     ack()
-    say(f"<@{body['user']['id']}> clicked the button")
+    # 組み込みのクライアントで views_open を呼び出し
+    client.views_open(
+        # 受け取りから 3 秒以内に有効な trigger_id を渡す
+        trigger_id=body["trigger_id"],
+        # ビューのペイロード
+        view={
+            "type": "modal",
+            # ビューの識別子
+            "callback_id": "view_1",
+            "title": {"type": "plain_text", "text":"My App"},
+            "submit": {"type": "plain_text", "text":"Submit"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {"type": "mrkdwn", "text":"Welcome to a modal with _blocks_"},
+                    "accessory": {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text":"Click me!"},
+                        "action_id": "button_abc"
+                    }
+                },
+                {
+                    "type": "input",
+                    "block_id": "input_c",
+                    "label": {"type": "plain_text", "text":"What are your hopes and dreams?"},
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "dreamy_input",
+                        "multiline":True
+                    }
+                }
+            ]
+        }
+    )
 
 @app.message("predict")
 def message_predict(_, say):
