228c29c0bb6992db3bc5c7589e00cb74db35603a
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-04-17 12:39:34 +0000

    レスポンスの形式を変更

--- bee_slack_app/repository_external_api.py
+++ bee_slack_app/repository_external_api.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 import requests  # type: ignore
 
@@ -10,7 +10,7 @@ class _BookSearch:
     url = "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404"
     APPLICATION_ID = os.environ["RAKU_APP_ID"]
 
-    def search_by_title(self, target_title: str) -> Tuple[int, dict[str, str]]:
+    def search_by_title(self, target_title: str) -> Tuple[int, List[dict[str, str]]]:
         """
         タイトルから書籍を検索する
 
@@ -18,7 +18,7 @@ class _BookSearch:
             title : 検索したい書籍のタイトル（曖昧検索も可能）
         Returns:
             hits: 検索でヒットした件数
-            dict: ヒットした書籍のタイトル(key)とISBN(value)の辞書形式データ
+            list: ヒットした書籍の辞書形式データをリストで格納する
         """
         # URLのパラメータ
         param = {
@@ -36,17 +36,22 @@ class _BookSearch:
 
         # print(json_result)
 
-        # 整形した結果を格納する辞書型変数を宣言
-        dict_result = {}
+        # 整形した結果を格納するリスト型変数を宣言
+        list_result = []
 
         # 取得結果を1件ずつ取り出す
         for value in json_result["Items"]:
-            item = value["Item"]
+            _item = value["Item"]
 
-            # keyに「タイトル（title）」、valueに「ISBN（isbn）」を設定する
-            dict_result[item["title"]] = item["isbn"]
+            # 必要な情報を辞書に格納する
+            dict_item = {
+                "title": _item["title"],
+                "isbn": _item["isbn"],
+                "author": _item["author"],
+            }
+            list_result.append(dict_item)
 
-        return json_result["hits"], dict_result
+        return json_result["hits"], list_result
 
     def search_by_isbn(self, isbn: int) -> Tuple[bool, Optional[str]]:
         """
@@ -72,7 +77,7 @@ class _BookSearch:
         # jsonにデコードする
         json_result = result.json()
 
-        print(json_result)
+        # print(json_result)
 
         if json_result["hits"] != 1:
             return False, None
@@ -84,10 +89,15 @@ bookSearch = _BookSearch()
 
 
 # Usage example
-hits, titles = bookSearch.search_by_title("仕事ではじめる機械学習")
 
-print(f"hits: {hits} \ntitles: \n  {titles}")
+print("===== search by title('仕事ではじめる') =====")
 
+hits, items = bookSearch.search_by_title("仕事ではじめる")
+print(f"hits: {hits}")
+for item in items:
+    print(f"title: {item['title']}  isbn: {item['isbn']}  author: {item['author']}")
+
+print("===== search by isbn('9784873119472') =====")
 
 hits, title = bookSearch.search_by_isbn(9784873119472)
 
