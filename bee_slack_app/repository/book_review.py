e33fd224e17f4e4ac1e8658b0c306040e9520c97
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-04-20 15:28:51 +0000

    dynamidbを取得する処理を切り出した

--- bee_slack_app/repository/book_review.py
+++ bee_slack_app/repository/book_review.py
@@ -1,16 +1,11 @@
 import os
 
-import boto3  # type: ignore
-
+from bee_slack_app.repository.database import dynamodb
 
 # This is a sample
 class BookReview:
     def __init__(self):
-        # 環境変数にDYNAMODB_ENDPOINTが設定されていればそこに接続する（ローカル環境）
-        # 設定されていなければデフォルトのDynamoDBに接続する（AWSの環境）
-        # TODO: この処理はアプリ全体で共通のため1か所に集めたい
-        endpoint_url = os.getenv("DYNAMODB_ENDPOINT", None)
-        dynamodb = boto3.resource("dynamodb", endpoint_url=endpoint_url)
+
         self.table = dynamodb.Table(os.environ["DYNAMODB_TABLE"])
 
     def create(self, review):
---
+++ bee_slack_app/repository/database.py
@@ -0,0 +1,8 @@
+import os
+
+import boto3  # type: ignore
+
+# 環境変数にDYNAMODB_ENDPOINTが設定されていればそこに接続する（ローカル環境）
+# 設定されていなければデフォルトのDynamoDBに接続する（AWSの環境）
+endpoint_url = os.getenv("DYNAMODB_ENDPOINT", None)
+dynamodb = boto3.resource("dynamodb", endpoint_url=endpoint_url)
