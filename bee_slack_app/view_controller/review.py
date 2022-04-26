d56439d1ae03603f0e6cbc0b80ac13d6ae4248c8
mitamurat <mita@esm.co.jp>
2022-04-26 02:22:43 +0000

    import文のsort対応

--- bee_slack_app/view_controller/review.py
+++ bee_slack_app/view_controller/review.py
@@ -1,5 +1,5 @@
 from bee_slack_app.model.review import ReviewContents
-from bee_slack_app.service.review import post_review, get_review_all
+from bee_slack_app.service.review import get_review_all, post_review
 
 
 def review_controller(app):
