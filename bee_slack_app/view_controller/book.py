84c1b87aa44af94f808c96ff95170c7d2105401c
Hiroki SAKABE <hiroki.sakabe@esm.co.jp>
2022-04-27 11:56:23 +0900

    今日のおすすめ本モーダルを作成（中身は未実装）

---
+++ bee_slack_app/view_controller/book.py
@@ -0,0 +1,29 @@
+# pylint: disable=duplicate-code
+
+
+def book_controller(app):
+    @app.action("see_more_recommended_book")
+    def open_see_more_recommended_book_modal(ack, body, client):
+        """
+        今日のおすすめ本モーダル
+        """
+        ack()
+        client.views_open(
+            trigger_id=body["trigger_id"],
+            view_id=body["view"]["id"],
+            hash=body["view"]["hash"],
+            view={
+                "type": "modal",
+                "title": {"type": "plain_text", "text": "今日のおすすめ本", "emoji": True},
+                "close": {"type": "plain_text", "text": "閉じる", "emoji": True},
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "ごめんなさい :bow: \nここは未実装です!!\n\nみなさんのレビューがたまれば、\nそれを元に、あなたにおすすめの本を表示します!!",
+                        },
+                    }
+                ],
+            },
+        )
