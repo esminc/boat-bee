2f2b9d8bab7e045126ee00a1f6aa62655224fae4
mitamurat <mita@esm.co.jp>
2022-04-29 03:49:00 +0000

    レコード追加のケースを追加

--- bee_slack_app/repository/test_user_profile.py
+++ bee_slack_app/repository/test_user_profile.py
@@ -25,7 +25,7 @@ class TestUserProfile:
             ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
         )
 
-    def test_レビューを作成できること(self):
+    def test_ユーザー情報を作成できること(self):
         response = self.table.query(
             KeyConditionExpression=boto3.dynamodb.conditions.Key("user_id").eq(
                 "test_user_id"
@@ -40,7 +40,7 @@ class TestUserProfile:
             {
                 "user_id": "test_user_id",
                 "user_name": "永和　太郎",
-                "department": "ＩＴＳ",
+                "department": "ＩＴＳ事業部",
                 "job_type": "技術職",
                 "age_range": "20",
                 "updated_at": "2022-04-01T00:00:00+09:00",
@@ -121,4 +121,52 @@ class TestUserProfile:
         assert actual["department"] == "金融システム事業部"
         assert actual["job_type"] == "管理職"
         assert actual["age_range"] == "50"
+        assert actual["updated_at"] == "2022-04-28T09:32:14+09:00"
+
+    def test_ユーザー情報を追加できること(self):
+        user_profile = UserProfile()
+
+        user_profile.create(
+            {
+                "user_id": "test_user_id",
+                "user_name": "永和 花子",
+                "department": "金融システム事業部",
+                "job_type": "営業職",
+                "age_range": "30",
+                "updated_at": "2022-04-15T09:20:12+09:00",
+            }
+        )
+
+        response = self.table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key("user_id").eq(
+                "test_user_id"
+            ),
+        )
+
+        assert len(response["Items"]) == 1
+
+        actual = response["Items"][0]
+
+        assert actual["user_id"] == "test_user_id"
+        assert actual["user_name"] == "永和 花子"
+        assert actual["department"] == "金融システム事業部"
+        assert actual["job_type"] == "営業職"
+        assert actual["age_range"] == "30"
+        assert actual["updated_at"] == "2022-04-15T09:20:12+09:00"
+
+        response = self.table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key("user_id").eq(
+                "test_user_id_1"
+            ),
+        )
+
+        assert len(response["Items"]) == 1
+
+        actual = response["Items"][0]
+
+        assert actual["user_id"] == "test_user_id_1"
+        assert actual["user_name"] == "追加　小次郎"
+        assert actual["department"] == "金融システム事業部"
+        assert actual["job_type"] == "技術職"
+        assert actual["age_range"] == "40"
         assert actual["updated_at"] == "2022-04-28T09:32:14+09:00"
