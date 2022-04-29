308e9476ab20c33e7b8ad8c7ab69a80bb17fab37
mitamurat <mita@esm.co.jp>
2022-04-29 04:43:40 +0000

    機能のコメントを追加

--- bee_slack_app/repository/user_profile.py
+++ bee_slack_app/repository/user_profile.py
@@ -3,16 +3,17 @@ import os
 from bee_slack_app.model.user import User
 from bee_slack_app.repository.database import get_database_client
 
-
-# This is a sample
+# ユーザー情報の追加・更新・削除を行います
 class UserProfile:
     def __init__(self):
         self.table = get_database_client().Table(os.environ["DYNAMODB_TABLE"])
 
+    # 全てのデータを抽出します
     def get_all(self):
         # 未実装
         pass
 
+    # データを追加および上書きします
     def create(self, profile: User):
         item = {
             "user_id": profile["user_id"],
@@ -27,6 +28,7 @@ class UserProfile:
 
         return item
 
+    # データを削除します
     def delete(self):
         # 未実装
         pass
