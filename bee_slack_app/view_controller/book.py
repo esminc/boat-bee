57e9066885b9a94ff228f7a69fb39f257412fa92
Hiroki SAKABE <hiroki.sakabe@esm.co.jp>
2022-05-02 16:45:20 +0900

    wip

--- bee_slack_app/view_controller/book.py
+++ bee_slack_app/view_controller/book.py
@@ -16,12 +16,33 @@ def book_controller(app):
                 "close": {"type": "plain_text", "text": "閉じる", "emoji": True},
                 "blocks": [
                     {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "ウェブ最適化ではじめる機械学習",
+                            "emoji": True,
+                        },
+                    },
+                    {
+                        "type": "image",
+                        "image_url": "http://books.google.com/books/content?id=jQ0OzgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+                        "alt_text": "ウェブ最適化ではじめる機械学習",
+                    },
+                    {
+                        "type": "section",
+                        "text": {"type": "mrkdwn", "text": "*著者*\n飯塚修平"},
+                    },
+                    {
+                        "type": "section",
+                        "text": {"type": "mrkdwn", "text": "*ISBN*\n9784873119168"},
+                    },
+                    {
                         "type": "section",
                         "text": {
                             "type": "mrkdwn",
-                            "text": "ごめんなさい :bow: \nここは未実装です!!\n\nみなさんのレビューがたまれば、\nそれを元に、あなたにおすすめの本を表示します!!",
+                            "text": "<https://books.google.co.jp/books?id=jQ0OzgEACAAJ&dq=webæé©å&hl=&source=gbs_api|Google Booksで見る>",
                         },
-                    }
+                    },
                 ],
             },
         )
