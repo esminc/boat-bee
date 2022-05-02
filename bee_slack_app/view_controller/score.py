676fac68fff31cda50fa8020ba302c3c63eee3a0
mitamurat <mita@esm.co.jp>
2022-05-02 12:14:10 +0000

    no-self-useを対応

--- bee_slack_app/view_controller/score.py
+++ bee_slack_app/view_controller/score.py
@@ -4,22 +4,24 @@ DBのスコアを引数として受け取り、モーダルに表示する文言
 
 
 class ViewScore:
-    def score_for_me(self, score_key: str) -> str:
-        score_value = {
+    def __init__(self):
+        self.score_for_me_value = {
             "5": "とても良い",
             "4": "良い",
             "3": "普通",
             "2": "悪い",
             "1": "とても悪い",
         }
-        return score_value[score_key]
-
-    def score_for_others(self, score_key: str) -> str:
-        score_value = {
+        self.score_for_others_value = {
             "5": "とてもおすすめ",
             "4": "おすすめ",
             "3": "普通",
             "2": "おすすめしない",
             "1": "絶対におすすめしない",
         }
-        return score_value[score_key]
+
+    def score_for_me(self, score_key: str) -> str:
+        return self.score_for_me_value[score_key]
+
+    def score_for_others(self, score_key: str) -> str:
+        return self.score_for_others_value[score_key]
