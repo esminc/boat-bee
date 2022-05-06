d10f98c132df797fe09d1106386f4671692864c5
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-05-06 01:40:52 +0000

    検索結果をCacheして不要なAPIアクセスを抑止する

--- bee_slack_app/view_controller/book_search.py
+++ bee_slack_app/view_controller/book_search.py
@@ -1,4 +1,5 @@
-from typing import Optional, Tuple
+import json
+from typing import List, Optional, Tuple
 from bee_slack_app.service.book_search import search_book_by_title, search_book_by_isbn
 from bee_slack_app.view_controller.review import generate_review_input_modal_view
 
@@ -61,6 +62,7 @@ def book_search_controller(app):
                 )
             else:
                 search_list = []
+                cache_list = []
 
                 for book_result in book_results:
                     title = book_result["title"]
@@ -73,42 +75,106 @@ def book_search_controller(app):
                     }
                     search_list.append(search_item)
 
-                print(book_results[0])
+                    # ラジオボタン選択時に書籍のデータを再利用するため情報を保持しておく
+                    # とりあえずはISBNからURLを取得できれば良い
+                    # private_metadataに格納できる情報が3000文字なので最小限にする
+                    cache_item = {
+                        "isbn": book_result["isbn"],
+                        "image_url": book_result["image_url"],
+                        "google_books_url": book_result["google_books_url"],
+                    }
+                    cache_list.append(cache_item)
 
-                new_view = generate_search_result_model_view(options=search_list)
+                print(book_results[0])
+                print(f"search_list len = {len(json.dumps(cache_list))}")
+
+                # private_metadataに格納するためにCacheを文字列に変換する
+                cache_list_str = json.dumps(cache_list)
+
+                new_view = generate_search_result_model_view(
+                    options=search_list, cached_search_result=cache_list_str
+                )
                 client.views_push(trigger_id=body["trigger_id"], view=new_view)
 
-    def generate_search_result_model_view(options, selected_isbn=None):
+    def generate_search_result_model_view(
+        options,
+        selected_isbn: Optional[str] = None,
+        cached_search_result: Optional[str] = None,
+    ):
         """
         検索結果画面の作成を行う
 
         ISBNが与えられた場合はGoogle BooksへのURLと画像リンクを更新する
         """
+        # TODO: 暫定で適当な画像をデフォルトに設定、S3に画像を置くようになったら自前の画像に差し替える
+        dummy_url = "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg"
 
-        def isbn_to_urls(isbn: Optional[str]) -> Tuple[str, str]:
-            # TODO: 暫定で適当な画像をデフォルトに設定、S3に画像を置くようになったら自前の画像に差し替える
-            dummy_url = "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg"
+        def isbn_to_url_by_api(isbn: Optional[str]) -> Tuple[str, str]:
+            """
+            APIを用いてISBNからURLを取得する
+
+            Cacheから取得できない場合にはこちらを使う
+            """
             if isbn is None:
                 return dummy_url, dummy_url
 
             book = search_book_by_isbn(isbn)
 
-            print(f"searched book = {book}")
             image_url = (
                 book["image_url"]
                 if book is not None and book["image_url"] is not None
                 else dummy_url
             )
-            google_books_url = book["google_books_url"]
+            google_books_url = (
+                book["google_books_url"]
+                if book is not None and book["google_books_url"] is not None
+                else dummy_url
+            )
+
             return google_books_url, image_url
 
-        google_books_url, image_url = isbn_to_urls(selected_isbn)
-        print(f"image_url = {image_url}")
+        def isbn_to_url_by_cache(
+            isbn: Optional[str], search_result: List[dict[str, str]]
+        ) -> Tuple[Optional[str], Optional[str]]:
+            """
+            Cacheを用いてISBNからURLを取得する
+            """
+            if isbn is None:
+                return dummy_url, dummy_url
+
+            books = [x for x in search_result if x["isbn"] == isbn]
+            if len(books) == 0:
+                return None, None
+            else:
+                book = books[0]
+
+            image_url = (
+                book.get("image_url", dummy_url)
+                if book["image_url"] is not None
+                else dummy_url
+            )
+            google_books_url = (
+                book.get("google_books_url", dummy_url)
+                if book["google_books_url"] is not None
+                else dummy_url
+            )
+
+            return google_books_url, image_url
+
+        # private_metadataに格納していたCacheを文字列から復元する
+        search_resut = json.loads(cached_search_result)
+
+        google_books_url, image_url = isbn_to_url_by_cache(selected_isbn, search_resut)
+
+        # Cacheから取得できない場合はAPIを使って取得する
+        if google_books_url is None or image_url is None:
+            google_books_url, image_url = isbn_to_url_by_api(selected_isbn)
 
         view = {
             "type": "modal",
             # ビューの識別子
             "callback_id": "view_book_search",
+            "private_metadata": cached_search_result,
             "title": {
                 "type": "plain_text",
                 "text": "本の検索結果",
@@ -167,8 +233,11 @@ def book_search_controller(app):
         print(f"isbn = {isbn}")
         print(f"options = {options}")
 
+        cache_list = body["view"]["private_metadata"]
+        print(f"private metadata = {cache_list}")
+
         new_view = generate_search_result_model_view(
-            options=options, selected_isbn=isbn
+            options=options, selected_isbn=isbn, cached_search_result=cache_list
         )
         print(f"new_view = {new_view}")
 
