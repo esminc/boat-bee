2153d0c5c6d6a094d9d6966338009c3280fe19b9
k-misawa <k-misawa@esm.co.jp>
2022-05-06 01:55:31 +0000

    repositoryに合わせて引数名等を修正

--- bee_slack_app/service/test_user.py
+++ bee_slack_app/service/test_user.py
@@ -2,24 +2,23 @@ from bee_slack_app.model.user import User
 from bee_slack_app.service.user import add_user
 
 
-def test_ユーザ情報を登録できること(monkeypatch):
-    def mock_add_user(arg):
-        return [
-            {
-                "user_id": "user_id_0",
-                "user_name": "えいわ　金融１",
-                "department": "fin",
-                "job_type": "engineer",
-                "age_range": "20",
-            },
-        ]
+def test_ユーザ情報を登録できること(self, monkeypatch):
+    def mock_add_user(self, _):
+        user = {
+            "user_id": "user_id_0",
+            "user_name": "えいわ　金融１",
+            "department": "fin",
+            "job_type": "engineer",
+            "age_range": "20",
+        }
+        return user
 
-    monkeypatch.setattr(UserProfile, "create", mock_add_user)
+    monkeypatch.setattr(User, "create", mock_add_user)
 
-    user_info = add_user()
+    user = add_user()
 
-    assert user_info[0]["user_id"] == "user_id_0"
-    assert user_info[0]["user_name"] == "えいわ　金融１"
-    assert user_info[0]["department"] == "fin"
-    assert user_info[0]["job_type"] == "engineer"
-    assert user_info[0]["age_range"] == "20"
+    assert user[0]["user_id"] == "user_id_0"
+    assert user[0]["user_name"] == "えいわ　金融１"
+    assert user[0]["department"] == "fin"
+    assert user[0]["job_type"] == "engineer"
+    assert user[0]["age_range"] == "20"
--- bee_slack_app/service/user.py
+++ bee_slack_app/service/user.py
@@ -3,23 +3,25 @@ import logging
 from typing import Any, Optional
 
 from bee_slack_app.model.user import User
-from bee_slack_app.repository.user_profile import UserProfile
+from bee_slack_app.repository.user_repository import UserRepository
 
-user_profile_repository = UserProfile()
+user_repository = UserRepository()
 
 
-def add_user(user_contents: User) -> None:
-    # 入力されたデータを使った処理を実行。このサンプルでは DB に保存する処理を行う
+def add_user(user: User) -> None:
+    """
+    ユーザ情報の登録・更新をする
+
+    """
 
     try:
-        # DB に保存
-        user_profile_repository.create(
+        user_repository.create(
             {
-                "user_id": user_contents["user_id"],
-                "book_title": user_contents["user_name"],
-                "isbn": user_contents["department"],
-                "score_for_me": user_contents["job_type"],
-                "score_for_others": user_contents["age_range"],
+                "user_id": user["user_id"],
+                "book_title": user["user_name"],
+                "isbn": user["department"],
+                "score_for_me": user["job_type"],
+                "score_for_others": user["age_range"],
                 "updated_at": datetime.datetime.now(
                     datetime.timezone(datetime.timedelta(hours=9))
                 ).isoformat(timespec="seconds"),
@@ -27,5 +29,4 @@ def add_user(user_contents: User) -> None:
         )
 
     except Exception as error:  # pylint: disable=broad-except
-        # エラーをハンドリング
         logger.exception(f"Failed to store data {error}")
