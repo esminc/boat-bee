63d38853aa9e5ed28b058a0de096d99e156c1f6e
k-misawa <k-misawa@esm.co.jp>
2022-05-10 04:58:50 +0000

    lintとtypeのエラー対応

--- bee_slack_app/view_controller/review.py
+++ bee_slack_app/view_controller/review.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from dateutil import parser  # type: ignore
 
 from bee_slack_app.model.review import ReviewContents
@@ -57,6 +55,7 @@ def review_controller(app):
             "score_for_me": score_for_me,
             "score_for_others": score_for_others,
             "review_comment": review_comment,
+            "updated_at": None,
         }
 
         post_review(logger, review_contents)
