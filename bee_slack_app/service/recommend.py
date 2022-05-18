e750489977ee01253462418220d0160b49fed245
mitamurat <mita@esm.co.jp>
2022-05-18 00:34:41 +0000

    用語を統一（ブックを本に）

--- bee_slack_app/service/recommend.py
+++ bee_slack_app/service/recommend.py
@@ -7,16 +7,16 @@ from bee_slack_app.repository.google_books import GoogleBooks
 
 def recommend(logger: Any, user: User) -> Optional[SearchedBook]:
     """
-    おすすめのブック情報を返却する
+    おすすめの本の情報を返却する
 
     Args:
         user : おすすめ本を知りたい、利用者のユーザ情報。
 
     Returns:
-        book: おすすめするブック情報。取得できない場合は、Noneが返る。
+        book: おすすめする本の情報。取得できない場合は、Noneが返る。
     """
     # TODO 機械学習の部品が実装できたら、部品を呼び出す。
-    # TODO 取得したisbnからブック情報を取得する。
+    # TODO 取得したisbnから本の情報を取得する。
     print(user)
     isbn = "9784873118253"
 
