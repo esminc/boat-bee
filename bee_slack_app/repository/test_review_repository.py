7ec6e1dce0aa36a7fc674f26a34533dbad7a63fd
Hiroki SAKABE <hiroki.sakabe@esm.co.jp>
2022-05-31 17:09:13 +0900

    リソースが存在しない場合のテストケースを追加

--- bee_slack_app/repository/test_review_repository.py
+++ bee_slack_app/repository/test_review_repository.py
@@ -109,6 +109,59 @@ class TestReview:
         assert reviews[1]["book_author"] == "dummy_book_author_0"
         assert reviews[1]["book_url"] == "dummy_book_url_0"
 
+    def test_テーブルに存在しないレビューのISBNを指定した場合_空配列を返すこと(self):  # pylint: disable=invalid-name
+        item = {
+            "user_id": "user_id_0",
+            "book_title": "仕事ではじめる機械学習",
+            "isbn": "12345",
+            "score_for_me": "1",
+            "score_for_others": "5",
+            "review_comment": "とても良いです",
+            "updated_at": "2022-04-01T00:00:00+09:00",
+            "book_image_url": "dummy_book_image_url_0",
+            "book_author": "dummy_book_author_0",
+            "book_url": "dummy_book_url_0",
+        }
+
+        self.table.put_item(Item=item)
+
+        item = {
+            "user_id": "user_id_1",
+            "book_title": "仕事ではじめる機械学習",
+            "isbn": "12345",
+            "score_for_me": "3",
+            "score_for_others": "4",
+            "review_comment": "まあまあです",
+            "updated_at": "2022-04-01T00:00:00+09:00",
+            "book_image_url": "dummy_book_image_url_0",
+            "book_author": "dummy_book_author_0",
+            "book_url": "dummy_book_url_0",
+        }
+
+        self.table.put_item(Item=item)
+
+        item = {
+            "user_id": "user_id_2",
+            "book_title": "Python チュートリアル",
+            "isbn": "67890",
+            "score_for_me": "2",
+            "score_for_others": "4",
+            "review_comment": "そこそこです",
+            "updated_at": "2022-04-02T00:00:00+09:00",
+            "book_image_url": "dummy_book_image_url_2",
+            "book_author": "dummy_book_author_2",
+            "book_url": "dummy_book_url_2",
+        }
+
+        self.table.put_item(Item=item)
+
+        review_repository = ReviewRepository()
+
+        reviews = review_repository.get_by_isbn(isbn="isbn_not_exist")
+
+        assert len(reviews) == 0
+        assert isinstance(reviews, list)
+
     def test_レビューを一意に指定して取得できること(self):
         item = {
             "user_id": "user_id_0",
