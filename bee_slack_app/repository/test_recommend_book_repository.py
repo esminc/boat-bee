ce4bba94bf736298de1379ae2d4851b7bce15eea
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-06-01 09:28:18 +0000

    テストを修正

--- bee_slack_app/repository/test_recommend_book_repository.py
+++ bee_slack_app/repository/test_recommend_book_repository.py
@@ -6,16 +6,18 @@ from bee_slack_app.repository.recommend_book_repository import RecommendBookRepo
 def test_複数のおすすめ本を取得できること(mocker):
     mocker.patch.object(
         RecommendBookRepository,
-        "_load_recommend_book_dict",
+        "_load_recommend_info",
         return_value={
-            "user_id_0": {
-                "ML-a": "1234567890123",
-                "ML-b": "2345678901234",
-            },
-            "user_id_1": {
-                "ML-a": "9876543210987",
-                "ML-b": "8765432109876",
-            },
+            "result": {
+                "user_id_0": {
+                    "ML-a": "1234567890123",
+                    "ML-b": "2345678901234",
+                },
+                "user_id_1": {
+                    "ML-a": "9876543210987",
+                    "ML-b": "8765432109876",
+                },
+            }
         },
     )
 
@@ -32,8 +34,10 @@ def test_複数のおすすめ本を取得できること(mocker):
 def test_おすすめ本の取得で存在しないユーザIDに対してはNoneを返すこと(mocker):  # pylint: disable=invalid-name
     mocker.patch.object(
         RecommendBookRepository,
-        "_load_recommend_book_dict",
-        return_value={"user_id_0": "1234567890123", "user_id_1": "1234567890456"},
+        "_load_recommend_info",
+        return_value={
+            "result": {"user_id_0": "1234567890123", "user_id_1": "1234567890456"},
+        },
     )
 
     recommend_book_repository = RecommendBookRepository()
@@ -46,15 +50,17 @@ def test_おすすめ本の取得で存在しないユーザIDに対してはNon
 def test_ひとつのMLがNoneでもおすすめ本を取得できること(mocker):  # pylint: disable=invalid-name
     mocker.patch.object(
         RecommendBookRepository,
-        "_load_recommend_book_dict",
+        "_load_recommend_info",
         return_value={
-            "user_id_0": {
-                "ML-b": "2345678901234",
-            },
-            "user_id_1": {
-                "ML-a": "9876543210987",
-                "ML-b": "8765432109876",
-            },
+            "result": {
+                "user_id_0": {
+                    "ML-b": "2345678901234",
+                },
+                "user_id_1": {
+                    "ML-a": "9876543210987",
+                    "ML-b": "8765432109876",
+                },
+            }
         },
     )
 
