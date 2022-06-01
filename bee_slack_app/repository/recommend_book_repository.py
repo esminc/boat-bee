81d02491757f781efc7a76bb35e590cc8af1b3ce
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-06-01 09:21:43 +0000

    メタデータ（タイムスタンプ）付きのJSONに対応

--- bee_slack_app/repository/recommend_book_repository.py
+++ bee_slack_app/repository/recommend_book_repository.py
@@ -3,7 +3,10 @@ from typing import Optional
 
 
 class RecommendBookRepository:  # pylint: disable=too-few-public-methods
-    recommend_book_dict = None
+    recommend_info = None
+
+    def __init__(self) -> None:
+        self.recommend_info = self._load_recommend_info()
 
     def fetch(self, user_id: str) -> Optional[dict[str, str]]:
         """
@@ -14,12 +17,10 @@ class RecommendBookRepository:  # pylint: disable=too-few-public-methods
         Returns:
            MLモデルとおすすめの本のISBNを辞書形式で返す
         """
-        if not self.recommend_book_dict:
-            self.recommend_book_dict = self._load_recommend_book_dict()
-        return self.recommend_book_dict.get(user_id)
 
-    @staticmethod
-    def _load_recommend_book_dict():
+        return self.recommend_info["result"].get(user_id)
+
+    def _load_recommend_info(self):
         with open(
             "./ml/models/recommended_book.json", encoding="utf-8", mode="r"
         ) as json_file:
