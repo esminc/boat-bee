7ba74c521f3b9bc509e7a0eb1a152e77fa41309e
mitamurat <mita@esm.co.jp>
2022-06-03 09:22:47 +0000

    複数ボタンの切り替え

--- bee_slack_app/view/test_button.py
+++ bee_slack_app/view/test_button.py
@@ -1,4 +1,10 @@
-def generate_test_button_model_view(callback_id: str, interested: bool):
+import json
+
+
+def generate_test_button_model_view(
+    callback_id: str,
+    interested: list[bool],
+):
     """
     ボタンのテスト用モーダル
 
@@ -6,7 +12,28 @@ def generate_test_button_model_view(callback_id: str, interested: bool):
         callback_id: モーダルのcallback_id
     """
     blocks = []
-    button_name = "興味あり１" if interested else "興味なし１"
+
+    for i, button_status in enumerate(interested):
+        create_button(button_status, blocks, i)
+
+    interested_dict = {"interested": interested}
+
+    # private_metadataに格納するために文字列に変換する
+    private_metadata = json.dumps(interested_dict)
+
+    view = {
+        "type": "modal",
+        "callback_id": callback_id,
+        "private_metadata": private_metadata,
+        "title": {"type": "plain_text", "text": "ボタンの切り替えテスト"},
+        "close": {"type": "plain_text", "text": "閉じる", "emoji": True},
+        "blocks": blocks,
+    }
+    return view
+
+
+def create_button(interested: bool, blocks: list, button_value: int):
+    button_name = "興味あり" if interested else "興味なし"
     blocks.append(
         {
             "type": "actions",
@@ -18,19 +45,9 @@ def generate_test_button_model_view(callback_id: str, interested: bool):
                         "text": button_name,
                         "emoji": True,
                     },
-                    "value": "dummy_value",
+                    "value": str(button_value),
                     "action_id": "test_button_switch_action",
                 },
             ],
         },
     )
-
-    view = {
-        "type": "modal",
-        "callback_id": callback_id,
-        "private_metadata": button_name,
-        "title": {"type": "plain_text", "text": "ボタンの切り替えテスト"},
-        "close": {"type": "plain_text", "text": "閉じる", "emoji": True},
-        "blocks": blocks,
-    }
-    return view
--- bee_slack_app/view_controller/test_home_button.py
+++ bee_slack_app/view_controller/test_home_button.py
@@ -1,3 +1,4 @@
+import json
 from bee_slack_app.view.test_button import generate_test_button_model_view
 
 
@@ -8,7 +9,7 @@ def test_home_button_controller(app):  # pylint: disable=too-many-statements
         modal_view = generate_test_button_model_view(
             # 興味なしボタンを表示
             callback_id="test_button_modal",
-            interested=False,
+            interested=[False, False, False],
         )
 
         client.views_open(
@@ -17,14 +18,22 @@ def test_home_button_controller(app):  # pylint: disable=too-many-statements
         )
 
     @app.action("test_button_switch_action")
-    def handle_some_action(ack, body, logger, client):
+    def handle_updete_button_action(ack, body, logger, client):
         private_metadata = body["view"]["private_metadata"]
+        # private_metadataに格納していた情報を復元する
+        interested_dict = json.loads(private_metadata)
+        interested_list = interested_dict["interested"]
+        value_data = int(body["actions"][0]["value"])
+
+        # 押されたボタンを反転させる
+        interested_list[value_data] = not interested_list[value_data]
+
         ack()
         logger.info(body)
         modal_view = generate_test_button_model_view(
             # 興味ありボタンの表示を切り替える
             callback_id="test_button_modal",
-            interested=True if private_metadata == "興味なし１" else False,
+            interested=interested_list,
         )
         client.views_update(
             view_id=body["container"]["view_id"],
