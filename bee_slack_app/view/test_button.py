ba4334fe0fcc515cc429c2fc1caac67a6f2ca338
mitamurat <mita@esm.co.jp>
2022-06-03 02:41:47 +0000

    興味ありボタンのテスト実装

--- bee_slack_app/view/test_button.py
+++ bee_slack_app/view/test_button.py
@@ -1,23 +1,34 @@
-def generate_test_button_model_view(callback_id: str):
+def generate_test_button_model_view(callback_id: str, interested: bool):
     """
-    おすすめ本モーダル
+    ボタンのテスト用モーダル
 
     Args:
         callback_id: モーダルのcallback_id
     """
     blocks = []
+
     blocks.append(
         {
-            "type": "image",
-            "image_url": "https://developers.google.com/maps/documentation/images/powered_by_google_on_white.png",
-            "alt_text": "",
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "興味あり１" if interested else "興味なし１",
+                        "emoji": True,
+                    },
+                    "value": "dummy_value",
+                    "action_id": "test_button_non_1_action",
+                },
+            ],
         },
     )
 
     view = {
         "type": "modal",
         "callback_id": callback_id,
-        "title": {"type": "plain_text", "text": "あなたへのおすすめ本"},
+        "title": {"type": "plain_text", "text": "ボタンの切り替えテスト"},
         "close": {"type": "plain_text", "text": "閉じる", "emoji": True},
         "blocks": blocks,
     }
--- bee_slack_app/view_controller/test_home_button.py
+++ bee_slack_app/view_controller/test_home_button.py
@@ -5,9 +5,25 @@ def test_home_button_controller(app):  # pylint: disable=too-many-statements
     @app.action("test_home_button_action")
     def open_test_home_button_modal(ack, body, client):
         ack()
-        modal_view = generate_test_button_model_view(callback_id="test_button_modal")
+        modal_view = generate_test_button_model_view(
+            callback_id="test_button_modal", interested=False
+        )
 
         client.views_open(
             trigger_id=body["trigger_id"],
+            view=modal_view,
+        )
+
+    @app.action("test_button_non_1_action")
+    def handle_some_action(ack, body, logger, client):
+        print("body=", body)
+        ack()
+        logger.info(body)
+        print("button_test_print")
+        modal_view = generate_test_button_model_view(
+            callback_id="test_button_modal", interested=True
+        )
+        client.views_update(
+            view_id=body["container"]["view_id"],
             view=modal_view,
         )
