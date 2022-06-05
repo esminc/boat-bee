f9bd4a0a8344ad6bb7df3c8c656c1508d8c7a2bc
mitamurat <mita@esm.co.jp>
2022-06-05 07:41:20 +0000

    suggestedのserviceを作成

---
+++ bee_slack_app/service/suggested_test.py
@@ -0,0 +1,25 @@
+# pylint: disable=non-ascii-name
+
+from bee_slack_app.repository.suggested_book_repository import SuggestedBookRepository
+from bee_slack_app.service.suggested import get_suggested
+
+
+def test_おすすめ本の情報を取得を取得できること(monkeypatch):
+    def mock_suggested_repository_get(_, __):
+        return {
+            "user_id": "test_user_id",
+            "isbn": "1234567890123",
+            "ml_model": "test_ml_model",
+            "interested": False,
+            "updated_at": "2022-05-02T16:43:25+09:00",
+        }
+
+    monkeypatch.setattr(SuggestedBookRepository, "get", mock_suggested_repository_get)
+
+    suggested = get_suggested("test_user_id", "1234567890123", "test_ml_model")
+
+    # assert suggested["user_id"] == "test_user_id"
+    assert suggested["isbn"] == "1234567890123"
+    assert suggested["ml_model"] == "test_ml_model"
+    assert suggested["interested"] is False
+    assert suggested["updated_at"] == "2022-05-02T16:43:25+09:00"
