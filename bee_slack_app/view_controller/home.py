417d5f400c84ac05ff10b3a81db306e9af8637b1
mitamurat <mita@esm.co.jp>
2022-06-07 01:36:01 +0000

    興味ありボタンのview_controllerを作成

--- bee_slack_app/view_controller/home.py
+++ bee_slack_app/view_controller/home.py
@@ -38,6 +38,17 @@ def home_controller(app):  # pylint: disable=too-many-statements
 
         recommended_books = recommend(user)
 
+        # TODO: ボタン状態はDBから読み出す
+        # とりあえず今は固定値で始める
+        # button_status_list = [False, False, False]
+        button_status_list = []
+        print("recommended_user=", event["user"])
+        for recommended_book in recommended_books:
+            print("recommended_isbn=", recommended_book[0]["isbn"])
+            print("recommended_ml_model=", recommended_book[1])
+            button_status_list.append(False)
+        print("recommended_status=", button_status_list)
+
         books_params = None
         metadata_str = ""
 
@@ -59,6 +70,8 @@ def home_controller(app):  # pylint: disable=too-many-statements
         client.views_publish(
             user_id=event["user"],
             view=home(
+                callback_id="suggested_button_modal",
+                button_status_list=button_status_list,
                 post_review_action_id="post_review_action",
                 recommended_books=recommended_books,
                 user_info_action_id="user_info_action",
@@ -97,6 +110,8 @@ def home_controller(app):  # pylint: disable=too-many-statements
 
         recommended_books = recommend(user)
 
+        button_status_list = [False, False, False]
+
         books_params = None
         metadata_str = ""
 
@@ -118,6 +133,8 @@ def home_controller(app):  # pylint: disable=too-many-statements
         client.views_publish(
             user_id=user_id,
             view=home(
+                callback_id="suggested_button_modal",
+                button_status_list=button_status_list,
                 post_review_action_id="post_review_action",
                 recommended_books=recommended_books,
                 user_info_action_id="user_info_action",
@@ -152,6 +169,8 @@ def home_controller(app):  # pylint: disable=too-many-statements
 
         recommended_books = recommend(user)
 
+        button_status_list = [False, False, False]
+
         metadata_dict = _PrivateMetadataConvertor.to_dict(
             private_metadata=private_metadata
         )
@@ -177,6 +196,8 @@ def home_controller(app):  # pylint: disable=too-many-statements
         client.views_publish(
             user_id=user_id,
             view=home(
+                callback_id="suggested_button_modal",
+                button_status_list=button_status_list,
                 post_review_action_id="post_review_action",
                 recommended_books=recommended_books,
                 user_info_action_id="user_info_action",
@@ -187,6 +208,75 @@ def home_controller(app):  # pylint: disable=too-many-statements
                 private_metadata=metadata_str,
             ),
         )
+
+    @app.action("button_switch_action")
+    def handle_updete_button_action(ack, body, event, client):
+
+        ack()
+        logger = getLogger(__name__)
+
+        reviews = get_review_all()
+
+        total_review_count = len(reviews) if reviews else 0
+        recommend_timestamp = parse(created_at())
+
+        logger.info({"total_review_count": total_review_count})
+
+        user = get_user(body["user"]["id"])
+        user_name = f"{user['user_name']}さん" if user is not None else "あなた"
+
+        recommended_books = recommend(user)
+
+        books_params = None
+        metadata_str = ""
+
+        books = get_books(limit=BOOK_NUMBER_PER_PAGE, keys=[])
+
+        logger.info({"books": books})
+
+        if books:
+            books_params = {
+                "books": books.get("items"),
+                "show_move_to_back": False,
+                "show_move_to_next": books.get("has_next"),
+            }
+
+            metadata_str = _PrivateMetadataConvertor.to_private_metadata(
+                keys=books.get("keys")
+            )
+
+        # private_metadataに格納していた情報を復元する
+        private_metadata = body["view"]["private_metadata"]
+        button_info = json.loads(private_metadata)
+        button_status_list = button_info["interested"]
+
+        # どのボタンが押されたか判定する
+        # valueにはstrしか格納できないためintに戻して利用する
+        button = int(body["actions"][0]["value"])
+
+        # 押されたボタンを反転させる
+        button_status_list[button] = not button_status_list[button]
+
+        # 興味ありボタンの表示を切り替える
+        modal_view = home(
+            callback_id="suggested_button_modal",
+            button_status_list=button_status_list,
+            post_review_action_id="post_review_action",
+            recommended_books=recommended_books,
+            user_info_action_id="user_info_action",
+            total_review_count=total_review_count,
+            user_name=user_name,
+            recommend_timestamp=recommend_timestamp,
+            books_params=books_params,
+            private_metadata=metadata_str,
+        )
+        client.views_update(
+            view_id=body["container"]["view_id"],
+            view=modal_view,
+        )
+
+        # TODO: 最新のボタン状態をDBに格納する
+        print("recommended_status=", button_status_list)
 
 
 class _PrivateMetadataConvertor:
