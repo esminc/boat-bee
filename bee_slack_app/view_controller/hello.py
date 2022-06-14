018721a3d50afa9f9fc5c6ade6c269b9a0359041
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-06-14 12:10:32 +0000

    レビューした本の数を表示

--- bee_slack_app/view_controller/hello.py
+++ bee_slack_app/view_controller/hello.py
@@ -19,10 +19,11 @@ def hello_controller(app):
             x["book_title"]
             for x in review_service.get_reviews_by_user_id(user_id=user_id)
         ]
+        num_reviews = len(myreviews)
         myreviews = "\n".join(myreviews)
 
         client.chat_postEphemeral(
             channel=os.environ["NOTIFY_POST_REVIEW_CHANNEL"],
             user=user_id,
-            text=f"<@{user_id}>さんが投稿したレビューはこれです\n{myreviews}",
+            text=f"<@{user_id}>さんが投稿したレビューはこれです（{num_reviews}冊）\n{myreviews}",
         )
