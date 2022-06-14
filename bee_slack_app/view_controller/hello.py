d85100ebe034830387b07089d7e1647c6028ce76
OKAMOTO Takuya <takuya.okamoto@esm.co.jp>
2022-06-14 11:58:40 +0000

    自分の投稿したレビュー一覧をチャットに表示する

--- bee_slack_app/view_controller/hello.py
+++ bee_slack_app/view_controller/hello.py
@@ -1,5 +1,28 @@
+import os
+
+from bee_slack_app.service import review_service
+
+
 def hello_controller(app):
     @app.message("hello")
     def message_hello(message, say):
         # say() sends a message to the channel where the event was triggered
         say(f"Hey there!! <@{message['user']}>!")
+
+    @app.message("myreview")
+    def show_my_review(ack, message, client):
+
+        ack()
+
+        user_id = message["user"]
+        myreviews = [
+            x["book_title"]
+            for x in review_service.get_reviews_by_user_id(user_id=user_id)
+        ]
+        myreviews = "\n".join(myreviews)
+
+        client.chat_postEphemeral(
+            channel=os.environ["NOTIFY_POST_REVIEW_CHANNEL"],
+            user=user_id,
+            text=f"<@{user_id}>さんが投稿したレビューはこれです\n{myreviews}",
+        )
