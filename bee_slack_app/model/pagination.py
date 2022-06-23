8975392f45b6d8172f2de4607e4d48dc16a9fb18
mitamurat <mita@esm.co.jp>
2022-06-23 07:07:55 +0000

    ReviewPaginationの型ヒントをlist[ReviewContents]に変更

--- bee_slack_app/model/pagination.py
+++ bee_slack_app/model/pagination.py
@@ -1,7 +1,9 @@
 from typing import TypedDict
 
+from bee_slack_app.model.review import ReviewContents
+
 
 class ReviewPagination(TypedDict):
-    reviews: list
+    reviews: list[ReviewContents]
     show_move_to_back: bool
     show_move_to_next: bool
