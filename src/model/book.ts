c66e73cec13d24e20a87bb4e64cf189084e3529a
Hiroki SAKABE <hiroki.sakabe@esm.co.jp>
2022-09-17 12:41:24 +0000

    wip

---
+++ src/model/book.ts
@@ -0,0 +1,12 @@
+/**
+ * 本
+ */
+export type Book = {
+  isbn: string;
+  title: string;
+  author: string;
+  url: string;
+  image_url: string;
+  description: string;
+  updated_at: string;
+};
--- src/model/index.ts
+++ src/model/index.ts
@@ -1,3 +1,4 @@
 export * from "./review";
 export * from "./user";
 export * from "./pagination";
+export * from "./book";
