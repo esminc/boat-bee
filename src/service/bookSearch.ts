1ab7da9c14edfe3cbd92a6222a5a03b098b674aa
Hiroki SAKABE <hiroki.sakabe@esm.co.jp>
2022-09-18 08:27:47 +0000

    wip

--- src/service/bookSearch.ts
+++ src/service/bookSearch.ts
@@ -29,8 +29,21 @@ class BookSearchService {
   /**
    * ISBNから書籍を検索する
    */
-  async fetchByIsbn(isbn: string): Promise<SearchedBook | null> {
-    return null;
+  async fetchByIsbn(isbn: string): Promise<SearchedBook | undefined> {
+    const book = await googleBooksRepository.searchBookByIsbn(isbn);
+
+    if (!book) {
+      return undefined;
+    }
+
+    return {
+      title: book.title,
+      isbn: book.isbn,
+      authors: book.authors,
+      imageUrl: book.imageUrl,
+      googleBooksUrl: book.googleBooksUrl,
+      description: book.description,
+    };
   }
 }
 
